e0feac709698ef52381600faa793f03146030efc
Alok Kumar <35198015+alokkumarsunny@users.noreply.github.com>
2024-12-31 22:31:20 +0530

    setup

---
+++ src/main/java/com/nextwebspark/jwt_auth_api/JwtAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.nextwebspark.jwt_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextwebspark/jwt_auth_api/JwtAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextwebspark.jwt_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
